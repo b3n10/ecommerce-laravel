e6bde18a5cb386b8e7a1332847e99eb193940e03
b3n10 <francisben.lleve@gmail.com>
2018-10-08 15:36:55 +0800

    Add UserController and landing page

---
+++ resources/js/views/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+			<div class="container">
+				<router-link :to="{ name: 'home' }" class="navbar-brand">Ben's Store</router-link>
+				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggle-icon"></span>
+				</button>
+			</div>
+		</nav>
+	</div>
+</template>
